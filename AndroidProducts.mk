086c03045982ff3fb8205e5271c8eeea3e4b9227
Junior Andrade de Almedia <junioraalmeida@hotmail.com>
2019-12-22 12:04:28 -0300

    Bring up Lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,3 +5,12 @@ COMMON_LUNCH_CHOICES := \
     aosp_lavender-user \
     aosp_lavender-userdebug \
     aosp_lavender-eng
+
+# Mods by @juniiim
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/lineage_lavender.mk
+
+COMMON_LUNCH_CHOICES += \
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng
